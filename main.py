a053a9acfa27f723ae58ef3fb13faddae3b60fb6
Mariza <marizadima@yahoo.com>
2020-11-16 11:48:43 +0000

    Initial code

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-# integer
-position = 5
-# float..we write our comments here
-# a second line
-moving_step = 0.2
-name = "Orange"
-gameOn = False
-
-number = int(input("Give me a number: "))
-
-if number <= 100:
-    print("The number is small")
-elif number <= 1000:
-    print("the number is medium")
-else:
-    print("The number is adequately big")
+numbers = [1,23,55,66,100,55.5]
+colours = ["red", "orange", "blue", "yellow"]
+mixed_literals = ["yellow", 34, 67.9]
+numbers.append(678)
 
 
 
 
 
-
-# print(name)
-#
-# print("My position is %d, my moving step is %.2f and my name is %s" % (position, moving_step, name))
-# print("My position is {2}, my moving step is {0} and my name is {1}".format(position, moving_step, name))
 
