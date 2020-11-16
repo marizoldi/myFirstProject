41204e9ae888fec5a23d81bf053067caa761ce4d
Mariza <marizadima@yahoo.com>
2020-11-16 16:48:03 +0000

    added tuples

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-numbers = [1,23,55,66,100,55.5]
-colours = ["red", "orange", "blue", "yellow"]
-mixed_literals = ["yellow", 34, 67.9]
-numbers.append(678)
+# myTuple = (6,7,8)
+# myTupleString = ("Name", "Age")
+# mixedTuple = (4,5.6, "name", (4,5))
+# uniqueElementTuple = (45,)
+#
+# print(len(myTuple))
+# print(myTuple[:2])
+#
+# x,y,z = myTuple
+# print(x)
 
-myDict = {"name": "marta", "Age": 3}
+myDict = { "Axe": 24, "Sword": 45, "Dagger": 5, "Degree": "Games"}
+print(myDict)
+myDict["Grades"] = ["A", "B", "B+"]
+myDict["Axe"] = 70
+print(myDict)
 
-
-
-
-
-
+for k,v in myDict.items():
+    print(k,v)
